9e695e7154db7bc5c86e234058a2074baab192d4
James Cavallo <jcavallo@colgate.edu>
2020-12-28 21:55:51 -0500

    Started building transactions objects

--- util/index.js
+++ util/index.js
@@ -1,5 +1,15 @@
 const EC = require('elliptic').ec;
+const cryptoHash = require('./crypto-hash');
 
 const ec = new EC('secp256k1');
 
-module.exports = {ec};
+const verifySignature = ({publicKey, data, signature}) => {
+    //creates key object from key
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+    //uses ec object to verify a signature based on hashed data
+    return keyFromPublic.verify(cryptoHash(data), signature);
+
+}
+
+module.exports = {ec, verifySignature, cryptoHash};
--- wallet/index.js
+++ wallet/index.js
@@ -1,15 +1,27 @@
 const {STARTING_BALANCE} = require('../config');
-const {ec} = require('../util');
+const {ec, cryptoHash} = require('../util');
 
 class Wallet{
     constructor(){
+        //amount in the wallet
         this.balance = STARTING_BALANCE;
 
         //generates private and public key pair with elliptic
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
 
-        this.publicKey = keyPair.getPublic().encode('hex');
+        //assigns new public key to the field 
+        this.publicKey = this.keyPair.getPublic().encode('hex');
     }
+
+    sign(data){
+        //encrypt to a hex hash
+        data = cryptoHash(data);
+
+        //returns the signed data
+        return this.keyPair.sign(data);
+    }
+
+
 
 }
 
