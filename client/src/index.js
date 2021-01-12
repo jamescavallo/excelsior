78510799c1c8065229746a00126d96c839ac86b2
James Cavallo <jcavallo@colgate.edu>
2021-01-11 20:51:29 -0500

    Created App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import App from './components/App';
 
-render(<div>Cryptochain in React!</div>, document.getElementById('root'));
-
-
-console.log('hi');
+render(<App></App>, 
+    document.getElementById('root'));
